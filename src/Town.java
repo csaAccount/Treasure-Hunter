1636f4cfad22f6a3c0d572a64dfda1244498c83d
BT_1N3_32 <bths123@bths.edu>
2023-12-20 09:15:47 -0500

    Added Marsh terrian, boots, and allowed for 6 items to be held in the inventory. Ticket 4

--- src/Town.java
+++ src/Town.java
@@ -124,15 +124,17 @@ public class Town {
      */
     private Terrain getNewTerrain() {
         double rnd = Math.random();
-        if (rnd < .2) {
+        if (rnd < .166666666666) {
             return new Terrain("Mountains", "Rope");
-        } else if (rnd < .4) {
+        } else if (rnd < .333333333) {
             return new Terrain("Ocean", "Boat");
-        } else if (rnd < .6) {
+        } else if (rnd < .5) {
             return new Terrain("Plains", "Horse");
-        } else if (rnd < .8) {
+        } else if (rnd < .666666666666) {
             return new Terrain("Desert", "Water");
-        } else {
+        } else if (rnd < .833333333333) {
+            return new Terrain("Marsh", "Boots");
+        }else {
             return new Terrain("Jungle", "Machete");
         }
     }
